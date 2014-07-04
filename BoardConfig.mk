3e4aa9165281ad3ee1765343b832b18946c1d392
xNUTx <nut@fun-industries.nl>
2014-07-04 11:44:01 +0200

    Ripping out the kernel building...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,4 +21,5 @@ include device/sony/pollux-common/BoardConfigCommon.mk
 # Assert
 TARGET_OTA_ASSERT_DEVICE := SGP311,SGP312,pollux_windy
 
-TARGET_KERNEL_CONFIG := cm_fusion3_pollux_windy_defconfig
+#TARGET_KERNEL_CONFIG := cm_fusion3_pollux_windy_defconfig
+TARGET_PREBUILT_KERNEL := device/sony/pollux/prebuilt/boguskernel
